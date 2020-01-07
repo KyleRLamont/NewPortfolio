b4ea90512a0cfa629227e7cee90607a79810d75a
Kyle LaMont <kylerlamont@gmail.com>
2020-01-06 16:37:04 -0800

    readded unused code, as removing it broke image loader

--- page/script/main.js
+++ page/script/main.js
@@ -130,62 +130,62 @@ $('.skill-list-item-level span').each(function()
 /******************************************************************************/
 
 try
-// {
-// 	var coordinate=new google.maps.LatLng(29.803621,-95.37811);
+{
+	var coordinate=new google.maps.LatLng(29.803621,-95.37811);
 
-// 	var mapOptions= 
-// 	{
-// 		zoom:15,
-// 		center:coordinate,
-// 		streetViewControl:false,
-// 		mapTypeControl:false,
-// 		zoomControlOptions: 
-// 		{
-// 			position:google.maps.ControlPosition.RIGHT_CENTER
-// 		},
-// 		panControlOptions: 
-// 		{
-// 			position:google.maps.ControlPosition.LEFT_CENTER
-// 		},
-// 		mapTypeId:google.maps.MapTypeId.ROADMAP
-// 	};
+	var mapOptions= 
+	{
+		zoom:15,
+		center:coordinate,
+		streetViewControl:false,
+		mapTypeControl:false,
+		zoomControlOptions: 
+		{
+			position:google.maps.ControlPosition.RIGHT_CENTER
+		},
+		panControlOptions: 
+		{
+			position:google.maps.ControlPosition.LEFT_CENTER
+		},
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
 
-// 	var googleMap=new google.maps.Map(document.getElementById('map'),mapOptions);
+	var googleMap=new google.maps.Map(document.getElementById('map'),mapOptions);
 
-// 	var markerOptions=
-// 	{
-// 		map:googleMap,
-// 		position:coordinate,
-// 		icon:mainURL+'image/map_marker.png'
-// 	}
+	var markerOptions=
+	{
+		map:googleMap,
+		position:coordinate,
+		icon:mainURL+'image/map_marker.png'
+	}
 
-// 	new google.maps.Marker(markerOptions);
+	new google.maps.Marker(markerOptions);
 	
-// 	$(window).resize(function() 
-// 	{
-// 		try {googleMap.setCenter(coordinate);}
-// 		catch(e) {}
-// 	});	
-// }
-// catch(e) {}	
+	$(window).resize(function() 
+	{
+		try {googleMap.setCenter(coordinate);}
+		catch(e) {}
+	});	
+}
+catch(e) {}	
 
 // /******************************************************************************/
 /*	Contact form															  */
 /******************************************************************************/
 
-// $('#contact-form').submit(function() 
-// {
-// 	submitContactForm();
-// 	return(false);
-// });
+$('#contact-form').submit(function() 
+{
+	submitContactForm();
+	return(false);
+});
 
-// $('#contact-form-reset').bind('click',function(e) 
-// {
-// 	e.preventDefault();
-// 	$('#contact-form').find('input[type="text"],textarea').val('').blur();
-// });
+$('#contact-form-reset').bind('click',function(e) 
+{
+	e.preventDefault();
+	$('#contact-form').find('input[type="text"],textarea').val('').blur();
+});
 
-// $('label.infield').inFieldLabels();
+$('label.infield').inFieldLabels();
 
 /******************************************************************************/
 /******************************************************************************/
