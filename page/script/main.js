2c6832c29a4ef0d245c35036a16074738ec560f5
Kyle LaMont <kylerlamont@gmail.com>
2020-01-06 16:34:59 -0800

    correction did not work, but added back image script.

--- page/script/main.js
+++ page/script/main.js
@@ -65,30 +65,30 @@ $('.gallery-list img').captify();
 /*	Hover for portfolio images												  */
 /******************************************************************************/
 
-// $('.gallery-list').hover(
+$('.gallery-list').hover(
 
-//     function() {},
-//     function()
-//     {
-//         $(this).find('li img').animate({opacity:1},250);
-//     }	
+    function() {},
+    function()
+    {
+        $(this).find('li img').animate({opacity:1},250);
+    }	
 
-// );
+);
 
-// $('.gallery-list li').hover(
+$('.gallery-list li').hover(
 
-//     function() 
-//     {
-//         $(this).siblings('li').find('img').css('opacity',0.5);
-//         $(this).find('img').animate({opacity:1},250);
-//     },
+    function() 
+    {
+        $(this).siblings('li').find('img').css('opacity',0.5);
+        $(this).find('img').animate({opacity:1},250);
+    },
 
-//     function()
-//     {
-//         $(this).find('img').css('opacity',1);	
-//     }
+    function()
+    {
+        $(this).find('img').css('opacity',1);	
+    }
 
-// );
+);
 	
 /******************************************************************************/
 /*	Portfolio filter														  */
